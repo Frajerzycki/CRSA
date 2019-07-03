9af2e0c39e0b11bf8ab22da7dfd7a08708dde531
frajerzycki <frajerzycki@protonmail.com>
2019-07-03 12:40:24 +0200

    Encryption and decryption without padding works.

--- RSA.h
+++ RSA.h
@@ -8,3 +8,10 @@ void RSA_generate_keys(int bits, RSA_key public_key, RSA_key private_key);
 
 // Init constants needed for RSA encryption. Should be called at the beginning of the program.
 void RSA_init();
+//
+void RSA_encrypt(char* message, char* ciphertext, size_t bytes_to_encrypt, size_t *encrypted_bytes, RSA_key public_key);
+//
+void RSA_decrypt(char* ciphertext, char* message, size_t bytes_to_decrypt, size_t *decrypted_bytes, RSA_key public_key, RSA_key private_key);
+//
+size_t RSA_size(RSA_key public_key);
+
--- example.c
+++ example.c
@@ -1,4 +1,5 @@
 #include <gmp.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "RSA.h"
 
@@ -8,6 +9,14 @@ int main(int argc, char* argv[]) {
   RSA_init();
   RSA_key private_key, public_key; 
   RSA_generate_keys(atoi(argv[1]), public_key, private_key);
+  char* message = "Kot";
+  char* ciphertext = malloc(RSA_size(public_key));
+  size_t encrypted_bytes;
+  RSA_encrypt(message, ciphertext, 4, &encrypted_bytes, public_key);
+  char* decrypted_message = malloc(RSA_size(public_key));
+  printf("%zu\n", encrypted_bytes);
+  RSA_decrypt(ciphertext, decrypted_message, encrypted_bytes, &encrypted_bytes, public_key, private_key);
+  printf("%s\n", decrypted_message); 
   gmp_printf("Public:\t%Zx\nPrivate:\t%Zx\n", public_key, private_key);
   return 0;
 }
