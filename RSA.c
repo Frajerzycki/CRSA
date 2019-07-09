2f679020df276a907f2bba781a50a0b4b0f27a2a
frajerzycki <frajerzycki@protonmail.com>
2019-07-09 11:15:46 +0200

    File formatted.

--- RSA.c
+++ RSA.c
@@ -5,30 +5,30 @@
 #elif defined __APPLE__
 #include <stdlib.h>
 #endif
-#include <string.h>
 #include "RSA.h"
+#include <string.h>
 
 mpz_t e;
 
 void random_number(size_t bytes, mpz_t destination) {
-  char* buffer = malloc(bytes);
-  arc4random_buf(buffer, bytes); 
+  char *buffer = malloc(bytes);
+  arc4random_buf(buffer, bytes);
   mpz_import(destination, bytes, 1, sizeof(char), 0, 0, buffer);
   free(buffer);
 }
 
 void random_prime(int bits, mpz_t destination) {
   size_t bytes = bits >> 3;
-  int lastbit_index = bits-1;
+  int lastbit_index = bits - 1;
   do {
     random_number(bytes, destination);
     mpz_setbit(destination, lastbit_index);
-  } while(mpz_probab_prime_p(destination, 50) < 1); 
+  } while (mpz_probab_prime_p(destination, 50) < 1);
 }
 
 void RSA_generate_keys(int bits, RSA_key public_key, RSA_key private_key) {
   int prime_size = bits >> 1; // Size in bits of p and q.
-  
+
   mpz_t p; // Secret prime number.
   mpz_init(p);
   random_prime(prime_size, p);
@@ -36,7 +36,7 @@ void RSA_generate_keys(int bits, RSA_key public_key, RSA_key private_key) {
   mpz_t q; // Secret prime number.
   mpz_init(q);
   random_prime(prime_size, q);
-  
+
   mpz_mul(public_key, p, q);
   // Subtract one from primes, beacuse λ(n) = lcm(p − 1, q − 1);
   mpz_sub_ui(p, p, 1);
@@ -45,16 +45,17 @@ void RSA_generate_keys(int bits, RSA_key public_key, RSA_key private_key) {
   mpz_t ctf; // Carmichael's totient function λ(n).
   mpz_init(ctf);
 
-  mpz_lcm(ctf,p,q);
-  mpz_clears(p, q, NULL); 
+  mpz_lcm(ctf, p, q);
+  mpz_clears(p, q, NULL);
 
-  mpz_t gcd; // Not used variable, just for storing result of Extended Euclidean algorithm.
+  mpz_t gcd; // Not used variable, just for storing result of Extended Euclidean
+             // algorithm.
   mpz_inits(gcd, private_key, NULL);
 
   mpz_gcdext(gcd, private_key, NULL, e, ctf);
   mpz_clear(gcd);
   while (mpz_sgn(private_key) < 1)
-    mpz_add(private_key, private_key, ctf); 
+    mpz_add(private_key, private_key, ctf);
   mpz_clear(ctf);
 }
 
@@ -63,11 +64,10 @@ void RSA_init() {
   mpz_set_si(e, 65537);
 }
 
-size_t RSA_size(RSA_key public_key) {
-  return mpz_sizeinbase(public_key, 256);
-}
+size_t RSA_size(RSA_key public_key) { return mpz_sizeinbase(public_key, 256); }
 
-void RSA_encrypt(char* message, char* ciphertext, size_t bytes_to_encrypt, size_t *encrypted_bytes, RSA_key public_key) {
+void RSA_encrypt(char *message, char *ciphertext, size_t bytes_to_encrypt,
+                 size_t *encrypted_bytes, RSA_key public_key) {
   mpz_t m; // Message turned into number.
   mpz_init(m);
   mpz_import(m, bytes_to_encrypt, 1, sizeof(char), 0, 0, message);
@@ -75,11 +75,13 @@ void RSA_encrypt(char* message, char* ciphertext, size_t bytes_to_encrypt, size_
   mpz_init(c);
   mpz_powm_sec(c, m, e, public_key);
   mpz_clear(m);
-  mpz_export(ciphertext, encrypted_bytes, 1, sizeof(char), 0, 0, c); 
+  mpz_export(ciphertext, encrypted_bytes, 1, sizeof(char), 0, 0, c);
   mpz_clear(c);
 }
 
-void RSA_decrypt(char* ciphertext, char* message, size_t bytes_to_decrypt, size_t *decrypted_bytes, RSA_key public_key, RSA_key private_key) {
+void RSA_decrypt(char *ciphertext, char *message, size_t bytes_to_decrypt,
+                 size_t *decrypted_bytes, RSA_key public_key,
+                 RSA_key private_key) {
   mpz_t c; // Encrypted message.
   mpz_init(c);
   mpz_import(c, bytes_to_decrypt, 1, sizeof(char), 0, 0, ciphertext);
@@ -87,6 +89,6 @@ void RSA_decrypt(char* ciphertext, char* message, size_t bytes_to_decrypt, size_
   mpz_init(m);
   mpz_powm_sec(m, c, private_key, public_key);
   mpz_clear(c);
-  mpz_export(message, decrypted_bytes, 1, sizeof(char), 0, 0, m); 
+  mpz_export(message, decrypted_bytes, 1, sizeof(char), 0, 0, m);
   mpz_clear(m);
 }
